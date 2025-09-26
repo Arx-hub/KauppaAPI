6d8ffe0f2c9b598af024a025b712a05068c3133d
Arx-hub <arxhenda11@hotmail.com>
2025-09-26 09:38:19 +0300

    Update Program.cs
    code that creates, adds and posts.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using KauppaAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var Kauppakanta = new Kauppakanta();
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/asiakkaat", () => Kauppakanta.HaeAsiakkaat());
+
+app.MapPost("/asiakkaat", (Asiakas asiakas) =>
+{
+    Kauppakanta.LisaaAsiakas(asiakas.Nimi);
+    return Kauppakanta.HaeAsiakkaat();
+});
 
 app.Run();
